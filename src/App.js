d848331bf969cf73c64364fffe1140b785e88806
Carolina mora <carolinamorahernandez@hotmail.com>
2022-12-15 23:07:42 -0300

    Add state abled

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
+import React, { useState } from "react";
 import Input from "./components/Input";
 import Button from "./components/Button";
-import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [abled, setAbled] = useState(false);
 
   return (
       <form className="formulario">
-        <Input username={username} setUsername={setUsername} password={password} setPassword={setPassword} />
-        <Button username={username} password={password} />  
+        <Input setAbled={setAbled}/>
+        <Button abled={abled}/>  
       </form>
     );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 
-function Input({ username, setUsername, password, setPassword }) {
+function Input({setAbled}) {
+
+    const validarEstado = value => {
+        if(value === "252525")
+            setAbled(true)
+        else
+            setAbled(false)
+    }
+    
     return (
         <>
             <h3 className="mb-5 text-center">Desafío estado de componentes y eventos</h3>
             <section>
                 <div className="form-group mb-3">
                     <label>Nombre</label>
-                    <input id="username" type='text' value={username} className="form-control" placeholder="Ingresa tu nombre de usuario" onChange={(e) => setUsername(e.target.value)} />
+                    <input id="username" type='text' className="form-control" placeholder="Ingresa tu nombre de usuario" />
                 </div>
                 <div className="form-group mb-3">
                     <label>Password</label>
-                    <input id="password" type='password' value={password} className="form-control" placeholder="Ingresa tu contraseña" onChange={(e) => setPassword(e.target.value)} />
+                    <input id="password" type='password' className="form-control" placeholder="Ingresa tu contraseña" onChange={(e) => validarEstado(e.target.value)} />
                 </div>
             </section>
         </>
